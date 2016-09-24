09c79cd0f75d80a5dcbcf093613834b7bd40fc66
hontas <lundin.pontus@gmail.com>
2016-09-24 09:13:54 +0200

    add temlate for js config

---
+++ config/template.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  prop: "from config",
+  level1: {
+    prop: "config",
+    array: ["config"],
+    level2: { config: true }
+  },
+  overridden: "from config.js"
+};
